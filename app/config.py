ec94f1e5393b71ecdec698f1bf2f8a8f40c3b1d9
Chris Laskey <contact@chrislaskey.com>
2014-02-03 16:04:56 -0500

    Blocking out Flask-Security setup

--- app/config.py
+++ app/config.py
@@ -26,3 +26,9 @@ MAIL_PASSWORD = None
 MAIL_DEFAULT_SENDER = None
 MAIL_MAX_EMAILS = None
 MAIL_SUPPRESS_SEND = TESTING
+
+# Flask-Security
+# See: http://pythonhosted.org/Flask-Security/configuration.html
+SECURITY_PASSWORD_HASH = 'bcrypt'
+SECURITY_PASSWORD_SALT = 'SnK2na02nlnmsWsna01nvmac1SNAncKS9acn3m11aZmQEiAk9314jXma'
+SECURITY_EMAIL_SENDER = 'no-reply@localhost'
--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 #!/usr/lib/virtualenvs/dominion/bin/python
 
+# Create database
+# Load all app.models.* first, hooking them into db. Then
+# call db.create_all().
 from app.models import db
-from app.models import user
+from app.models.user import User
 db.create_all()
