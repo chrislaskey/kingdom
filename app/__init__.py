fa6a352f3ecf01ed5cdb7886a43157233c1d6404
Chris Laskey <contact@chrislaskey.com>
2014-02-03 14:24:59 -0500

    Moved API routes to app/api

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,5 @@
-from flask import Flask, g, request
-from flask.ext.restful import Api, Resource
+from flask import Flask
 
 app = Flask(__name__)
-api = Api(app)
 
-class HelloWorld(Resource):
-
-    def get(self):
-        return {'hello': 'world'}
-
-api.add_resource(HelloWorld, '/')
+from . import api
