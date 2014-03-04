ad8b0b0ba105d9d619568f5632bb05faaf1a492e
Chris Laskey <contact@chrislaskey.com>
2014-03-03 19:53:10 -0500

    Updated model names

--- app/security.py
+++ app/security.py
@@ -1,6 +1,6 @@
 from flask.ext.security import Security, SQLAlchemyUserDatastore
 from . import app
-from . models.security import db, User, Role
+from . models.users import db, User, Role
 
 
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
