e1076b4dedd63625a7d9ff21c071d80d9b42e0dc
Chris Laskey <contact@chrislaskey.com>
2014-03-05 14:40:28 -0500

    Create game form now saves to database

--- app/views/creategame.py
+++ app/views/creategame.py
@@ -1,6 +1,10 @@
-from flask import flash, redirect, render_template
+from datetime import datetime
+from flask import redirect, render_template
 from flask.ext.security import login_required
 from .. import app
+from .. models import db
+from .. models.db_helpers import db_add
+from .. models.games import Game
 from .. forms.creategameform import CreateGameForm
 
 
@@ -9,6 +13,21 @@ from .. forms.creategameform import CreateGameForm
 def create_game():
     form = CreateGameForm()
     if form.validate_on_submit():
-        flash(u'Successfully created new game', 'success')
+        save_create_game(form)
         return redirect('/')
     return render_template('create-game.html', create_game_form=form)
+
+
+def save_create_game(form):
+    new_game = Game(
+        status       = 'created',
+        name         = form.game_name.data,
+        type         = form.game_type.data,
+        description  = form.game_description.data,
+        date_created = datetime.utcnow()
+    )
+    db_add(
+        new_game,
+        u'New game successfully created.',
+        u'An error occured when trying to create new game.'
+    )
