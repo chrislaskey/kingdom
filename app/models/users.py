f3b8a917c62b653ac8294505353edf59e5d7c4dc
Chris Laskey <contact@chrislaskey.com>
2014-03-06 16:30:54 -0500

    Added Friends model

--- app/models/users.py
+++ app/models/users.py
@@ -4,6 +4,17 @@ from . games import games_users
 from . roles import roles_users
 
 
+class Friend(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    friend_id = db.Column(db.Integer(), db.ForeignKey('user.id'),
+        primary_key=True)
+    friend = db.relationship('User', foreign_keys='Friend.friend_id')
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id'),
+        primary_key=True)
+    user = db.relationship('User', foreign_keys='Friend.user_id')
+    request_status = db.Boolean()
+
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(255), unique=True)
@@ -19,3 +30,5 @@ class User(db.Model, UserMixin):
         backref = db.backref('users', lazy = 'dynamic'))
     games = db.relationship('Game', secondary = games_users,
         backref = db.backref('users', lazy = 'dynamic'))
+    friends = db.relationship('Friend', backref='Friend.friend_id',
+        primaryjoin='User.id==Friend.user_id', lazy='joined')
