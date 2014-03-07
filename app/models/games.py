c81a0dec884599af7d340c81935cc4297b6ee456
Chris Laskey <contact@chrislaskey.com>
2014-03-07 14:16:14 -0500

    Optimized two text fields in Game model

--- app/models/games.py
+++ app/models/games.py
@@ -14,9 +14,9 @@ class Game(db.Model):
         game status, values created|declined|started|withdrew|ended
     '''
     id = db.Column(db.Integer, primary_key=True)
-    status = db.Column(db.Text())
+    status = db.Column(db.String(255))
     name = db.Column(db.Text())
-    type = db.Column(db.Text())
+    type = db.Column(db.String(255))
     description = db.Column(db.Text())
     date_created = db.Column(db.DateTime())
     players = db.relationship('User', secondary = games_users,
