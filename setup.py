d8654118ac5c66c938e3ef1d34806cf057672f06
Chris Laskey <contact@chrislaskey.com>
2014-03-10 12:57:21 -0400

    Added additional test data, games

--- setup.py
+++ setup.py
@@ -3,8 +3,11 @@
 # Create database
 # Load all app.models.* first, hooking them into db. Then call db.create_all().
 
+from datetime import datetime
 from app.models import db
 from app.models.friends import Friend
+from app.models.games import Game, games_users
+from app.models.users import User
 from app.security import user_datastore
 from sys import argv
 
@@ -32,10 +35,36 @@ if command == 'test':
     user_datastore.create_user(email='player-three@chrislaskey.com',
         password='$2a$12$bSeMt/jhBiltPTxHHPzcHO/q.vaeN57l25RyFp4eDnow9jegOfEo6')
 
+    db.session.commit()
+
     db.session.add_all([
         Friend(user_id=1, friend_id=2),
         Friend(user_id=2, friend_id=1),
         Friend(user_id=1, friend_id=3)
+    ])
+
+    db.session.add_all([
+        Game(
+            name = 'Game 1',
+            type = 'classic',
+            description = 'First description',
+            date_created = datetime.utcnow(),
+            players = db.session.query(User).filter(User.id.in_([1,2])).all()
+        ),
+        Game(
+            name = 'Game 2',
+            type = 'classic',
+            description = 'Second description',
+            date_created = datetime.utcnow(),
+            players = db.session.query(User).filter(User.id.in_([2,3])).all()
+        ),
+        Game(
+            name = 'Game 3',
+            type = 'classic',
+            description = 'Third description',
+            date_created = datetime.utcnow(),
+            players = db.session.query(User).filter(User.id.in_([1,2,3])).all()
+        )
     ])
 
     db.session.commit()
