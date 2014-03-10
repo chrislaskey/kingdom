db6d6bf5f20dcf5f5afe8a10eda563f599ebc54e
Chris Laskey <contact@chrislaskey.com>
2014-03-10 12:59:17 -0400

    Added games data to home view

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -1,12 +1,22 @@
 from flask import render_template
-from flask.ext.security import login_required
+from flask.ext.security import login_required, current_user
 from .. import app
+
+from .. models import db
+from .. models.games import Game
+from .. models.users import User
 
 
 @app.route('/')
 @login_required
 def index():
-    return render_template('index.html')
+    games = db.session.query(Game)\
+        .filter(Game.players.contains(current_user)).all()
+    return render_template(
+        'index.html',
+        current_user = current_user,
+        games = games
+    )
 
 
 from . import creategame
