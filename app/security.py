6b7faf04c5328b40428bed6d7259807bec8fa694
Chris Laskey <contact@chrislaskey.com>
2014-03-11 13:19:01 -0400

    Moved security into its own dir

--- app/security.py
+++
@@ -1,8 +0,0 @@
-from flask.ext.security import Security, SQLAlchemyUserDatastore
-from . import app
-from . models.roles import db, Role
-from . models.users import User
-
-
-user_datastore = SQLAlchemyUserDatastore(db, User, Role)
-security = Security(app, user_datastore)
