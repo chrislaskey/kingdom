1671d10476b70e22689c8f5f7de48f8325fdb954
Chris Laskey <contact@chrislaskey.com>
2014-03-11 13:22:09 -0400

    Added the extended register form fields to security object

--- app/security/__init__.py
+++ app/security/__init__.py
@@ -2,11 +2,13 @@ from flask.ext.security import Security, SQLAlchemyUserDatastore
 from .. import app
 from .. models.roles import db, Role
 from .. models.users import User
+from . register import ExtendedConfirmRegisterForm
 
 
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 
 security = Security(
     app,
-    user_datastore
+    user_datastore,
+    confirm_register_form = ExtendedConfirmRegisterForm
 )
