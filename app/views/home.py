60a41828c9692331f709b2c3a4882f436b8cfd7c
Chris Laskey <contact@chrislaskey.com>
2014-03-11 13:23:56 -0400

    Moved logged in homepage view to separate file

---
+++ app/views/home.py
@@ -0,0 +1,26 @@
+from flask import render_template
+from flask.ext.security import login_required, current_user
+from .. import app
+from .. models import db
+from .. models.games import Game
+from .. models.users import User
+
+
+@app.route('/')
+@login_required
+def index():
+    return render_template(
+        'index.html',
+        current_user = current_user,
+        is_first_login = is_first_login(),
+        games = get_games()
+    )
+
+
+def get_games():
+    return db.session.query(Game)\
+        .filter(Game.players.contains(current_user)).all()
+
+
+def is_first_login():
+    return current_user.last_login_at == current_user.current_login_at
